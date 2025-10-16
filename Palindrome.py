48ccc7bf4651833744a50e15c979266a069b7f7a
harshsomankar123-tech <harshsomankar123@gmail.com>
2025-10-17 02:21:23 +0530

    Implement isPalindrome method in Solution class
    Fix the implementation of the isPalindrome method and remove duplicate code.
    
    Signed-off-by: harshsomankar123-tech <harshsomankar123@gmail.com>

---
+++ Palindrome.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isclass Solution:
+    def isPalindrome(self, x: int) -> bool:
+        a = x
+        c = 0
+        while a>0:
+            c=c*10 + a%10
+            a//=10
+        if c==x:
+         return True 
+        return False        
+        (self, x: int) -> bool:
+        a = x
+        c = 0
+        while a>0:
+            c=c*10 + a%10
+            a//=10
+        if c==x:
+         return True 
+        return False        
+        
